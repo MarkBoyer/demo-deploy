abc25e3484ae16ded2d463ef380f520256fa7291
Mark A. Boyer <mark.a.boyer@gmail.com>
2019-03-29 19:47:52 -0700

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<name-of-app>'));
+app.use(express.static(__dirname + '/dist/demo-deploy'));
 
 app.get('/*', function(req,res) {
-    
+
 res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
 });
 
